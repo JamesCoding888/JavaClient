ae4a96661047baac7036b16686d936e8c9ccea63
jamesliao <JamesCoding888@hotmail.com>
2021-06-04 19:09:20 +0800

    Java Client Practice for JavaWeb20210531

--- src/main/java/com/client/GetWebData.java
+++ src/main/java/com/client/GetWebData.java
@@ -3,10 +3,10 @@ package com.client;
 
 import java.net.URL;
 import java.util.Scanner;
-
+//
 public class GetWebData{
     public static void main(String[] args) throws Exception{
-        String path = "http://localhost:8080/JavaWeb20210603/index.html";
+        String path = "http://localhost:8080/index.html";
         URL url = new URL(path);
         String html = new Scanner(url.openStream())
                 .useDelimiter("\\A")
